ea28cbba8d4c3a87be2da1f6ba4e30e5d82a527c
Heng Li <lh3@live.co.uk>
2011-09-01 01:26:57 -0400

    a little bit code clean up

--- readfq.py
+++ readfq.py
@@ -1,22 +1,21 @@
 def readfq(fp): # this is a generator function
     last = None # this is a buffer keeping the last unprocessed line
-    while (True): # mimic closure; is it a bad idea?
-        if last is None: # the first record or a record following a fastq
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
             for l in fp: # search for the start of the next record
                 if l[0] in '>@': # fasta/q header line
                     last = l[:-1] # save this line
                     break
-        if last is None: raise StopIteration
-        name = last[1:].split()[0] # sequence name
-        seqs, last = [], None
+        if not last: break
+        name, seqs, last = last[1:].split()[0], [], None
         for l in fp: # read the sequence
-            if l[0] in "@+>":
+            if l[0] in '@+>':
                 last = l[:-1]
                 break
             seqs.append(l[:-1])
-        if last is None or last[0] != '+': # this is a fasta record
+        if not last or last[0] != '+': # this is a fasta record
             yield name, ''.join(seqs), None # yield a fasta record
-            if last is None: raise StopIteration
+            if not last: break
         else: # this is a fastq record
             seq, leng, seqs = ''.join(seqs), 0, []
             for l in fp: # read the quality
@@ -28,7 +27,7 @@ def readfq(fp): # this is a generator function
                     break
             if last: # reach EOF before reading enough quality
                 yield name, seq, None # yield a fasta record instead
-                raise StopIteration
+                break
 
 if __name__ == "__main__":
     import sys
