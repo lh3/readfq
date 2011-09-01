05c63848db65080609df4e7a0a1fb6da4aa46f73
Heng Li <lh3@live.co.uk>
2011-09-01 00:57:08 -0400

    simplify the python implementation
    The python implementation was translated from the Lua one which uses closure.
    Closure shares something with Python's generator function, but not the same.
    The direct translation thus looks a little nasty.
    
    Honestly, I like closure better.

--- readfq.py
+++ readfq.py
@@ -1,38 +1,35 @@
 def readfq(fp): # this is a generator function
     import string
-    finished, last = False, None
+    last = None # this is a buffer keeping the last unprocessed line
     while (True): # mimic closure; is it a bad idea?
-        if (finished): raise StopIteration # already hit the end-of-file (EOF)
-        if (last is None): # the first record or a record following a fastq
+        if last is None: # the first record or a record following a fastq
             for l in fp: # search for the start of the next record
-                if l[0] in '>@':
+                if l[0] in '>@': # fasta/q header line
                     last = l[:-1] # save this line
                     break
-        if last is None: # EOF
-            finished = True
-            raise StopIteration
-        name = last[1:].split()[0]
-        seqs, last, c = [], None, None
+        if last is None: raise StopIteration
+        name = last[1:].split()[0] # sequence name
+        seqs, last = [], None
         for l in fp: # read the sequence
-            c = l[0]
-            if c in "@+>":
+            if l[0] in "@+>":
                 last = l[:-1]
                 break
             seqs.append(l[:-1])
-        if (last is None): finished = True # EOF
-        if (c != '+'): yield name, ''.join(seqs), None # yield a fasta record
+        if last is None or last[0] != '+': # this is a fasta record
+            yield name, ''.join(seqs), None # yield a fasta record
+            if last is None: raise StopIteration
         else: # this is a fastq record
             seq, leng, seqs = ''.join(seqs), 0, []
             for l in fp: # read the quality
                 seqs.append(l[:-1])
                 leng += len(l) - 1
-                if (leng >= len(seq)): # have read enough quality
+                if leng >= len(seq): # have read enough quality
                     last = None
                     yield name, seq, ''.join(seqs); # yield a fastq record
                     break
-            if (last): # reach EOF before reading enough quality
-                finished = True
+            if last: # reach EOF before reading enough quality
                 yield name, seq, None # yield a fasta record instead
+                raise StopIteration
 
 if __name__ == "__main__":
     import sys
