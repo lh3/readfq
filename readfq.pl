760c262834ef4f1acff0e514515d9b000b7bda9e
sestaton <statonse@gmail.com>
2013-09-23 15:16:57 -0400

    Revert "modification to properly return name and comment"
    This reverts commit 3b11c0a894924d2bd842b25371cedd894902a069.

--- readfq.pl
+++ readfq.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/env perl
 
-use 5.010;
 use strict;
 use warnings;
 
@@ -21,8 +20,8 @@ sub readfq {
 			return;
 		}
 	}
-	my ($name, $comm) = /^.(\S+)(?:\s+)(\S+)/ ? ($1, $2) : 
-	                    /^.(\S+)/ ? ($1, '') : ('', '');
+	my $name = /^.(\S+\s+\S+)/? $1 :
+	           /^.(\S+)/? $1 : '';
 	my $seq = '';
 	my $c;
 	$aux->[0] = undef;
@@ -34,14 +33,14 @@ sub readfq {
 	}
 	$aux->[0] = $_;
 	$aux->[1] = 1 if (!defined($aux->[0]));
-	return ($name, $comm, $seq) if ($c ne '+');
+	return ($name, $seq) if ($c ne '+');
 	my $qual = '';
 	while (<$fh>) {
 		chomp;
 		$qual .= $_;
 		if (length($qual) >= length($seq)) {
 			$aux->[0] = undef;
-			return ($name, $comm, $seq, $qual);
+			return ($name, $seq, $qual);
 		}
 	}
 	$aux->[1] = 1;
@@ -49,10 +48,11 @@ sub readfq {
 }
 
 my @aux = undef;
-my ($name, $comm, $seq, $qual);
+my ($name, $seq, $qual);
 my ($n, $slen, $qlen) = (0, 0, 0);
-while (($name, $comm, $seq, $qual) = readfq(\*STDIN, \@aux)) {
+while (($name, $seq, $qual) = readfq(\*STDIN, \@aux)) {
 	++$n;
 	$slen += length($seq);
 	$qlen += length($qual) if ($qual);
 }
+print join("\t", $n, $slen, $qlen), "\n";
